b3e1aaa5e8992117a06131d6efe01812e0d8ee50
weihouang <willy200335@gmail.com>
2024-02-13 18:24:06 -0600

    sidebar fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Sidebar from "./component/Sidebar/Sidebar";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 export default function App() {
   return (
