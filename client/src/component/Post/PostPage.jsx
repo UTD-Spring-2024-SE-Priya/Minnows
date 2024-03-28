14181680ceff185ea8a09ba753f55cb4234e41ea
KKelly83 <kevin.kelly0803@gmail.com>
2024-03-27 20:24:25 -0500

    Update PostPage.jsx

--- client/src/component/Post/PostPage.jsx
+++ client/src/component/Post/PostPage.jsx
@@ -17,20 +17,30 @@ import PostItem from "./PostItem";
 
 export default function PostPage() {
   const [posts, setPosts] = useState([]);
-  //const [post, setPost] = useState({ title: "", content: ""})
-  //const { title, content } = post;
+  const [post, setPost] = useState({ title: "", body: ""})
+  const { title, body } = post;
 
   async function fetchPosts() {
     const { data } = await supabase.from('posts').select('*');
     setPosts(data);
-}
+  }
+
+  async function createPost() {
+    await supabase.from('posts').insert([{ title, body }]).single()
+    setPost({ title: "", body: ""});
+    fetchPosts();
+  }
   useEffect(() => {
     fetchPosts();
   }, []);
 
+  const click = () => {
+    alert(title)
+  }
+
   return (
-    <Box p={"1em"} overflowY="auto" position="relative" h={"100%"}>
-      <Box mt={"1em"} mb={"2em"}>
+    <Box p={"1em"} overflowY="auto" position="fixed" w = {"full"} spacing = {4} >
+      <Box p = {"2em"} mt={"1em"} mb={"3em"} bg = {"white"} borderRadius={"lg"} boxShadow={"lg"} position = {"relative"} top = {"0"} >
         <Heading>Name the of Thread</Heading>
         <InputGroup>
           <InputLeftElement pointerEvents="none">
@@ -42,17 +52,24 @@ export default function PostPage() {
           />
         </InputGroup>
       </Box>
-      <VStack w={"100%"} spacing={"1em"}>
-      {
-        posts.map(post => (
-          <PostItem
-          authorName = { post.author_id }
-            title = { post.title }  
-            content = { post.body }
-            />
-        ))
-      }    
-      </VStack>
+      <Box mt={"5em"}  positon = "relative" h={"calc(100vh - 5em)"}>
+        <VStack w={"100%"} spacing={"1em"}>
+        {
+          posts.map(post => (
+            <PostItem
+              authorName = { post.author_id }
+              title = { post.title }  
+              content = { post.body }
+              />
+          ))
+        }    
+        </VStack>
+      </Box>
+      <Box mt = {"5em"} w = {"full"} p={"1em"} boxShadow={"lg"} borderRadius={"lg"} bg={"white"} position = {"fixed"} bottom = {"0"}>
+        <Heading>Create new post</Heading>
+          <Input placeholder = "Title" title = {title} onChange={e =>setPost({...post, title: e.target.value})}/>
+          <Input placeholder = "Content" body = {body} onChange={e =>setPost({...post, body: e.target.value})}/>
+      </Box>
     </Box>
   );
 }
